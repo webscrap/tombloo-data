4ffc40a49bf61935d72dfae8552b090fd1ecef96
Eoetct Nahn <eotect@gmail.com>
2012-09-10 01:32:10 +0800

    Extent Tombloo.Service for supporting sharing images more than one at a time.

---
+++ script/32_Tombloo.Services.js
@@ -0,0 +1,100 @@
+//Tombloo.Service.o_post = Tombloo.Service.post;
+Tombloo.Service.extractors.register({
+		name : 'Photos from Selection',
+		ICON : 'chrome://tombloo/skin/photo.png',
+		check : function(ctx){
+			return ctx.selection;
+		},
+		extract : function(ctx){
+			var html = '';
+			if (typeof ctx.window.getSelection != "undefined") {
+				var sel = ctx.window.getSelection();
+				if (sel.rangeCount) {
+					var p = ctx.document.createElement("div");
+					for (var i = 0, len = sel.rangeCount; i < len; ++i) {
+						p.appendChild(sel.getRangeAt(i).cloneContents());
+					}
+					html = p.innerHTML;
+				}
+		    } else if (typeof ctx.document.selection != "undefined") {
+				if (ctx.document.selection.type == "Text") {
+					html = ctx.document.selection.createRange().htmlText;
+				}
+			}	
+			var images = new Array;
+			var re = /<img[^>]+src="([^"]+)"/g;
+			var baseUrl = ctx.href;
+			var matches;
+			while(matches = re.exec(html)) {
+				var img = matches[1];
+				if(img.match('^http')) {
+					images.push(resolveRelativePath(img,baseUrl));
+				}
+			}
+			if(images.length) {
+				return {
+					images	: images,
+					item	: ctx.title,
+					itemUrl : ctx.href,
+					type	: 'photo',
+					description : joinText(images,"\n"),
+				}
+			}
+			else {
+				return {
+					type    : 'quote',
+					item    : ctx.title,
+					itemUrl : ctx.href,
+					body    : createFlavoredString(ctx.window.getSelection()),
+					bodyHtml : html,
+				}
+			}
+		},
+});
+
+function post_next(ps,posters,images,index,end) {
+	var img = images[index];
+	ps.itemUrl = img;
+	if(index < end) {
+		index++;
+		if(img) {
+			return Tombloo.Service.o_post(ps,posters).addCallback(function() {
+				return post_next(ps,posters,images,index,end);
+			});
+		}
+		else {
+			return post_next(ps,posters,images,index,end);
+		}
+	}
+	if(img) {
+		return Tombloo.Service.o_post(ps,posters);
+	}
+	else {
+		return succeed({});
+	}
+}
+
+update(Tombloo.Service, {
+	o_post	:	Tombloo.Service.post,
+	post	:	function(ps,posters) {
+		/*
+		var m = ps.bodyHtml;
+		if(!m) {
+			return Tombloo.Service.o_post(ps,posters);
+		}
+		*/
+		if(ps.images && ps.images.length && ps.description) {
+			var images = ps.description.split("\n");
+			alert("Get " + (images ? images.length : '0') + " images.");
+			if(images.length){
+				ps.type = 'photo';
+				ps.description = '';
+				return post_next(ps,posters,images,0,images.length - 1);
+			}
+			else {
+				return succeed({});
+			}
+		}
+		return Tombloo.Service.o_post(ps,posters);
+	},
+});
