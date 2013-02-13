ee2a81e955ff22a1448e483316fa97b15823aaa5
Eoetct Nahn <eotect@gmail.com>
2013-02-13 23:56:47 +0800

    Update model tombloo-bridge.

--- script/22.model.tombloo-bridge.js
+++ script/22.model.tombloo-bridge.js
@@ -11,39 +11,36 @@ models.register({
 	post : function(oldps){
 		var ps = modelExt.createPost(oldps);
 	    var tag = joinText(ps.tags, ',');
-		var process = new Process(new LocalFile('/myplace/bin/tombloo-bridge'));
-		var profd =  DirectoryService.get('ProfD', IFile).path;
-		var args = [
-				"item=>" + ps.item,
-				'type=>' + ps.type,
-				'itemUrl=>' + ps.itemUrl,
-				'pageUrl=>' + ps.pageUrl,
-				'tags=>' + joinText(ps.tags,","),
-				'description=>' + (ps.description || ""),
-				'file=>'+ (ps.file ? ps.file.path : ""),
-				'profd=>' + profd,
-				'body=>' + ps.body,
-				'private=>' + (ps.private ? '1' : '0'),
-				'adult=>' + (ps.adult ? '1' : '0'),
-		];
+		var app = getPref('model.TomblooBridge.application');
+		var TopDir = DirectoryService.get('ProfD', IFile);
+		var profd =  TopDir.path;
+		if(!app) {
+			app = '/myplace/bin/tombloo-bridge';
+		}
+		if(!app.match(/\/|\\/)) {
+			TopDir.append('tombloo');
+			TopDir.append(app);
+			app = TopDir.path;
+		}
+		var process = new Process(new LocalFile(app));
 		var file = getTempFile('txt');	
+		var args = [
+				"<item>:\n\t" + ps.item,
+				"<type>:\n\t" + ps.type,
+				"<itemUrl>:\n\t" + ps.itemUrl,
+				"<pageUrl>:\n\t" + ps.pageUrl,
+				"<tags>:\n\t" + joinText(ps.tags,","),
+				"<description>:\n\t" + (ps.description || ""),
+				"<file>:\n\t"+ (ps.file ? ps.file.path : ""),
+				"<profd>:\n\t" + profd,
+				"<body>:\n\t" + ps.body,
+				"<private>:\n\t" + (ps.private ? "1" : "0"),
+				"<adult>:\n\t" + (ps.adult ? "1" : "0"),
+		];
 		putContents(file,joinText(args,"\n"),'UTF8');
-/*		
-		if(ps.body && ps.body.length > 100) {
-			var file = getTempFile('txt');	
-			putContents(file,joinText(args,"\n"),'UTF-16');
-			var outs =  new FileOutputStream(file,FileOutputStream.PR_WRONLY | FileOutputStream.PR_CREATE_FILE | FileOutputStream.PR_TRUNCATE, 420, -1);
-			outs.write(ps.body,ps.body.length);
-			outs.close();
-			args.push('body-file=>'+file.path);
-		}
-		else {
-			args.push('body=>' + ps.body);
-		}
-*/
 		process.run(
 			false,
-			[file.path],
+			[file.path,profd],
 			1
 			);
 		return succeed({});
