3798d16ef9537211c02a282ad68862f6011c1ff9
Eotect Nahn <eotect@gmail.com>
2014-04-26 00:14:11 +0800

    Update file: bridge/apply.sh

--- bridge/apply.sh
+++ bridge/apply.sh
@@ -4,5 +4,5 @@ for i in *.txt ; do
 	echo "Processing $i..."
 	echo -e "\n<profd>:" >>"$i";
 	echo -e "\t$HOME/.mozilla/firefox/profiles/afun/" >>"$i";
-	perl /myplace/workspace/perl/tombloo-bridge.pl "$i"
+	perl /myplace/workspace/perl/tombloo-bridge.pl "$i" "$@"
 done
