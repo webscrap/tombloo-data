0cd6abfb745348a41d6d8f0eab6f5c55a18f95c5
Eotect Nahn <eotect@gmail.com>
2014-05-11 02:21:25 +0800

    [model.BaiduPan] Upload images, not articles for photo posts.

--- script/22.model.baidu-pan.js
+++ script/22.model.baidu-pan.js
@@ -33,26 +33,26 @@ models.register({
 			/*
 			param={"from":"bm","records":[{"width":720,"height":2516,"url":"http://www.xxx5555.info/html/zhxz/15548/","title":"âã£ææ°ã®æ¬§ç¾æ ç ã£æååéâ03.08â_å¥å¥å§å§ç»¼åï¼·ï¼·ï¼·ï¼ï¼¸ï¼¸ï¼¸ï¼ï¼ï¼ï¼ï¼ï¼©ï¼®ï¼¦ï¼¯","res_src":"http://img789.com/images/2014/02/14/qqk06Tp.jpg","desc":"","type":"image"}]}
 			*/
-			return this.getTok(ps.pageUrl).addCallback(function(tok){
-				request(actionUrl, {
-					referrer    : ps.pageUrl ,
-					sendContent : {
-						param:	JSON.stringify({
-							"from":"bm",
-							"records":[{
-								"url":ps.pageUrl,
-								"title":ps.item,
-								"text":ps.description,
-								"res_src":ps.itemUrl,
-								"content":'<p><a href="' + ps.pageUrl + '">' + ps.item + '</a><br /></p><img src="' + ps.itemUrl + '">',
-								"type":"article",
-								"tags":ps.tags,
-							}],
-						}),
-						bdstoken: tok,
-				   },
-				 });
-            });
+			// return this.getTok(ps.pageUrl).addCallback(function(tok){
+				// request(actionUrl, {
+					// referrer    : ps.pageUrl ,
+					// sendContent : {
+						// param:	JSON.stringify({
+							// "from":"bm",
+							// "records":[{
+								// "url":ps.pageUrl,
+								// "title":ps.item,
+								// "text":ps.description,
+								// "res_src":ps.itemUrl,
+								// "content":'<p><a href="' + ps.pageUrl + '">' + ps.item + '</a><br /></p><img src="' + ps.itemUrl + '">',
+								// "type":"article",
+								// "tags":ps.tags,
+							// }],
+						// }),
+						// bdstoken: tok,
+				   // },
+				 // });
+            // });
             return this.getTok(ps.pageUrl).addCallback(function(tok){
 				request(actionUrl, {
 					referrer    : ps.pageUrl ,
@@ -60,10 +60,10 @@ models.register({
 						param:	JSON.stringify({
 							"from":"bm",
 							"records":[{
-								"width":960,
-								"height":639,
+								//"width":960,
+								//"height":639,
 								"url":ps.pageUrl,
-								"title":ps.pageUrl,
+								"title":'[' + joinText(ps.tags,',') +']' + ps.item ,
 								"res_src":ps.itemUrl,
 								"desc":ps.description,
 								"type":"image"}],
