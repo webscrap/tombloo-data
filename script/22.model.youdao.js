e77d8e7be84b2af79d34e1fc347bb12ede709a7f
Eotect Nahn <eotect@gmail.com>
2014-07-02 00:13:33 +0800

    Update model.youdao, posting error detection added.
    	Update file: script/22.model.youdao.js

--- script/22.model.youdao.js
+++ script/22.model.youdao.js
@@ -12,6 +12,7 @@ models.register({
 	POSTDATA=title=TITLE&url=http%3A%2F%2FURL&tags=TAG1%2CTAG2&note=DESC%0D%0ADESC
 	*/
 	post : function(oldps){
+		var self = this;
 		var ps = modelExt.createPost(oldps,'firefox');
 	    var tag = joinText(ps.tags, ',');
         return request('http://shuqian.youdao.com/manage?a=popwindow', {
@@ -23,7 +24,23 @@ models.register({
                 tags        : tag,
                 note        : ps.description || ps.body || ps.pageUrl,
            },
-        });
+        }).addCallback(function(res) {self.checkPost(res,ps);});;
+	},
+	checkPost : function(res) {
+		var r = res.responseText;
+		var self = this;
+		if(r.match(/new_bm_form_title/)) {
+			return succeed(res);
+		}
+		else if(r.match(/youdao\.com\/login/)) {
+			throw new Error("Post failed: User not login");
+		}
+		else if(ps.quiet) {
+			throw new Error("Post failed:\n " + r);
+		}
+		else {
+			throw new Error("Post failed:\n " + r);
+		}
 	},
 	
 });
